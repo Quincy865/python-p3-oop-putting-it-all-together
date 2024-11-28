86eac47e29276832649eac2432ba87eebef2f123
Quincy Michael <michaelkirima865@gmail.com>
2024-11-28 13:18:07 +0300

    Put it all tg

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = None
+        self.page_count = page_count  
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+            self._page_count = None
+        else:
+            self._page_count = value
+
+    def turn_page(self):
+        if self._page_count is not None:
+            print("Flipping the page...wow, you read fast!")
+        else:
+            print("Unable to flip pages, invalid page count.")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = None
+        self.size = size  
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            print("size must be an integer")
+            self._size = None
+        else:
+            self._size = value
+
+    def repair(self):
+        print("The shoe has been repaired.")
+        self.condition = 'New'  
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = 'New'  
